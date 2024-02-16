dfccadadd848201f3cb2dbe9c3f1046f3acf0a62
AF0dEv <afonsopro.business@gmail.com>
2024-02-16 13:13:15 +0000

    delete db access credentials

--- Python1/Marcos_PI0123_CRUD/marcoscrudiefp.py
+++ Python1/Marcos_PI0123_CRUD/marcoscrudiefp.py
@@ -5,10 +5,10 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.secret_key = 'iefp crud'
 
-app.config['MYSQL_HOST'] = '62.28.39.135'
-app.config['MYSQL_USER'] = 'Raquel'
-app.config['MYSQL_PASSWORD'] = 'Silva1234'
-app.config['MYSQL_DB'] = 'flask_app'
+app.config['MYSQL_HOST'] = 
+app.config['MYSQL_USER'] = 
+app.config['MYSQL_PASSWORD'] = 
+app.config['MYSQL_DB'] = 
 
 
 mysql = MySQL(app)
--- Python1/ProjTarefasFlask/config.py
+++
@@ -1,4 +0,0 @@
-mysql_database_user = 'Raquel'
-mysql_database_password = 'Silva1234'
-mysql_database_db = 'projtarefaflask'
-mysql_database_host = '62.28.39.135'
